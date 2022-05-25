8c0aad5ab0907af49941e4ec83a70296ccff41ed
JonasMarschick <62710324+JonasMarschick@users.noreply.github.com>
2022-05-25 16:25:59 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -76,16 +76,16 @@ def ChangePWM(self):
 Button1 = Button(gui, text='Clockwise Motor ', command = motorClockwise, bg='green2', height = 2, width = 15)
 Button1.grid(row=0,column=0, sticky = "NSEW" )
 
-Button3 = Button(gui, text='Counter Clockwise ', command = motorAntiClockwise, bg='deep sky blue', padx = 30, height = 2, width = 10)
+Button3 = Button(gui, text='Counter Clockwise ', command = motorAntiClockwise, bg='deep sky blue',  height = 2, width = 15)
 Button3.grid(row=0,column=1, sticky = "NSEW" )
 
-Button2 = Button(gui, text=' Motor  Stop', command = motorStop, bg='red', height = 2, width = 10)
+Button2 = Button(gui, text=' Motor  Stop', command = motorStop, bg='red', height = 2, width = 15)
 Button2.grid(row=0,column=2, sticky = "NSEW" )
 
-Text3 = Label(gui,text='Motor  RPM', bg = 'grey1', fg='#FFFFFF', height = 2, width = 10)#, padx = 10, pady = 10)
+Text3 = Label(gui,text='Motor  RPM', bg = 'grey1', fg='#FFFFFF', height = 2, width = 15)
 Text3.grid(row=1,column = 0 , columnspan=1 , sticky = "NSEW" )
 
-Scale1 = Scale(gui, from_=40, to=100, orient = HORIZONTAL, resolution = 1, command = ChangePWM, length=250, width=20)
+Scale1 = Scale(gui, from_=40, to=100, orient = HORIZONTAL, resolution = 1, command = ChangePWM, length=250, width=15)
 Scale1.grid(row=1,column=2, columnspan=3 , sticky = "NSEW" )
 
 ########################################
